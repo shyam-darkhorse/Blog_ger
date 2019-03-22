683f2d5bd4d4a2a2df5bebf97fab4639c5758d83
shyam-darkhorse <30753805+shyam-darkhorse@users.noreply.github.com>
2019-03-22 19:26:07 +0530

    php for authors page

--- author.php
+++ author.php
@@ -13,53 +13,17 @@ include('session.php');
 	if ($connection->connect_error) {
 		die("Connection failed: " . $connection->connect_error);
 	} 
-	if(isset($_GET['id']))
-	$authid= $_GET['id'];
-	else
-	header('Location: author.php');
-	$sql = "SELECT * from bloguser where user_id = '$authid'";
+	$sql = "SELECT * from bloguser ";
 	$result = $connection->query($sql);
+	$author = "";
 	while($row =$result->fetch_assoc())
-	{
-		$title1 = '<div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: \'70%\' }">
-            <p class="breadcrumbs" data-scrollax="properties: { translateY: \'30%\', opacity: 1.6 }"><span class="mr-2"><a href="index.php">Home</a></span> <span>Author Post</span></p>
-            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: \'30%\', opacity: 1.6 }">'.$row['title'].'</h1>
-          </div>';
-		 
-		  $namequery = "SELECT username from user where user_id = '$authid'";
+	{		$authid = $row['user_id'];
+		 $namequery = "SELECT username from user where user_id = '$authid'";
 		  $res = $connection->query($namequery);
-		  while($row1 =$res->fetch_assoc())
+		 if( $row1 =$res->fetch_assoc())
 		{
-			 
-			global $nameauth;
 			$nameauth= $row1['username'];
-		 $desc = '<div class="col-md-12 heading-section text-center ftco-animate">
-		            <h2 class="mb-1">'.$row1['username'].'</h2>
-		            <span class="d-block mb-4">'.$row['blogcount'].' Articles</span>
-		            <p>'.$row['authorbio'].'</p>
-		          </div>;';
-		}
-		$articles = '';
-		$sql2 = "SELECT * FROM blog order by createdon desc";
-		$result2 = $connection->query($sql2);
-		while($row2 =$result2->fetch_assoc())
-		{	$s = $authid;
-			
-			$temp = $row2['blogid'];
-			$temp = str_replace(" ","",$temp);
-			$files = glob("/wamp64/www/Blog_ger/images/uploads/$temp.*");
-			$eventimg=0;
-			for ($i=0; $i<count($files); $i++)
-			{
-				$eventimg=1;
-				$num = $files[$i];
-				$num = str_replace("/wamp64/www","",$num);
-			}
-			if($eventimg==0)
-			{
-				$num="images/uploads/blog.jpg";
-			}
-			$temp = $row2['userid'];
+			$temp = $authid;
 			$files = glob("/wamp64/www/Blog_ger/signup/profilepic/$temp.*");
 			$flag1=1;
 			for ($i=0; $i<count($files); $i++)
@@ -73,50 +37,25 @@ include('session.php');
 				$dp="signup/profilepic/img-avatar.png";
 			}
 			
+			$author .='<div class="author-wrap d-flex">
+    					<div class="img" style="background-image: url('.$dp.');"></div>
+    					<div class="text">
+    						<h3>'.$nameauth.'</h3>
+    						<span class="d-block">'.$row['blogcount'].' Articles</span>
+    						<p>'.$row['authorbio'].'</p>
+    						<p><a href="author-post.php?id='.$authid.'" class="btn btn-primary btn-outline-primary">View Articles</a></p>
+    					</div>
+    				</div>';
 			
-			$text = $row2['text'];
-			$text = substr($text,0,120);
-			$articles .= ' 
-   			<div class="col-md-4">
-    				<div class="blog-entry ftco-animate">
-							<a href="single.php?id='.$row2['blogid'].'" class="img img-2" style="background-image: url('.$num.');"></a>
-							<div class="text text-2 pt-2 mt-3">
-		          	<span class="big">'.$row2['tag'].'</span>
-	              <h3 class="mb-4"><a href="single.php?id='.$row2['blogid'].'">'.$row2['title'].'</a></h3>
-	              <p class="mb-4">'.$text.'</p>
-	              <div class="author mb-4 d-flex align-items-center">
-	            		<a href="author-post.php?id='.$row2['userid'].'" class="img" style="background-image: url('.$dp.');"></a>
-	            		<div class="ml-3 info">
-	            			<span>Written by</span>
-	            			<h3><a href="author-post.php?id='.$row2['userid'].'">'.$nameauth.'</a>, <span>'.$row2['createdon'].'</span></h3>
-	            		</div>
-	            	</div>
-	              <div class="meta-wrap align-items-center">
-	              	<div class="half order-md-last">
-		              	<p class="meta">
-		              		<span><i class="icon-heart"></i>'.$row2['likes'].'</span>
-		              		<span><i class="icon-eye"></i>'.$row2['views'].'</span>
-		              		<span><i class="icon-comment"></i>'.$row2['comments'].'</span>
-		              	</p>
-	              	</div>
-	              	<div class="half">
-		              	<p><a href="single.php?id='.$row2['blogid'].'" class="btn btn-primary px-3 py-2">Continue Reading</a></p>
-	              	</div>
-	              </div>
-	            </div>
-						</div>
-    			</div>
-				';
-
+		}
+		else{
+		echo "No records";
 		
 		}
-		
 	}
-
-						
-?>
+	?>
   <head>
-    <title>Author's page</title>
+    <title>BLogger | authors</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
@@ -144,7 +83,7 @@ include('session.php');
   </head>
   <body>
     
-	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
 	      <a class="navbar-brand" href="index.html">Blogger</a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
@@ -185,33 +124,33 @@ include('session.php');
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
-          <?php echo $title1;?>
+          <div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.php">Home</a></span> <span>Pages</span></p>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Authors Page</h1>
+          </div>
         </div>
       </div>
     </div>
 
-     <section class="ftco-section">
+    <section class="ftco-section">
     	<div class="container">
-    		<div class="row justify-content-center mb-5 pb-2">
-          <div class="col-md-7 heading-section text-center ftco-animate">
-            <?php echo $desc;?>
-        </div>
-    		<div class="row">
-    				
-    					
-		    					    		<?php echo $articles;?>
-    				</div>
-    		
-    			</div><!-- END-->
-    			
+    		<div class="row justify-content-center">
+    			<div class="col-md-8">
+				
+				<?php echo $author;?>
+					
+					
+					
+    			</div>
     		</div>
     	</div>
     </section>
 
     <footer class="ftco-footer ftco-bg-dark ftco-section">
       <div class="container">
-       
-        </div>
+	  
+
+		
         <div class="row">
           <div class="col-md-12 text-center">
 
--- single.php
+++ single.php
@@ -29,32 +29,20 @@
   </head>
   <body>
     
-	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.html">Explorer</a>
+	      <a class="navbar-brand" href="index.html">Blogger</a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home</a>
-              <div class="dropdown-menu" aria-labelledby="dropdown04">
-              	<a class="dropdown-item" href="index.html">Home with slider</a>
-                <a class="dropdown-item" href="index2.html">Home with full slider</a>
-                <a class="dropdown-item" href="index3.html">Home with parallax</a>
-              </div>
+	          <li class="nav-item"><a href="index.php" class="nav-link">Home</a>
             </li>
-	          <li class="nav-item active dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Post</a>
-              <div class="dropdown-menu" aria-labelledby="dropdown04">
-              	<a class="dropdown-item" href="post-right-sidebar.html">Post with right sidebar</a>
-                <a class="dropdown-item" href="post-left-sidebar.html">Post with left sidebar</a>
-                <a class="dropdown-item" href="post-no-sidebar.html">Post no sidebar</a>
-              </div>
+	          <li class="nav-item"><a href="author.php" class="nav-link">Authors</a>
             </li>
-	          <li class="nav-item dropdown">
+	      <!--    <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Archives</a>
               <div class="dropdown-menu" aria-labelledby="dropdown04">
               	<a class="dropdown-item" href="destination.html">Destination</a>
@@ -69,8 +57,9 @@
                 <a class="dropdown-item" href="left-sidebar.html">Left Sidebar</a>
                 <a class="dropdown-item" href="author.html">Authors Page</a>
               </div>
-            </li>
+            </li>-->
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+			   <li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
 	        </ul>
 	      </div>
 	    </div>
